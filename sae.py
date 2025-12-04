dbe043273da6a4bf80dbd3eb36e1f6da1face6d9
MirhaneYOUSSOUF <mirhane.youssouf@iut-dijon.u-bourgogne.fr>
2025-12-04 16:32:40 +0100

    Add files via upload

--- sae.py
+++ sae.py
@@ -1,5 +1,6 @@
 import csv
-
+from matplotlib import pyplot as plt
+from pylab import plot, figure, show
 table=[]
 with open('donnees_2008.csv',newline='') as csvfile:
 	reader=csv.reader(csvfile,delimiter=',')
@@ -23,9 +24,9 @@ with open('donnees_2016.csv',newline='') as csvfile:
 #print(donnees_2016)
 #print(table[1][4])
 
-for i in range(len(table)):
-	if table[i][6]=='Auxerre':
-		print('Auxerre', table[i][9])
+for i in range(len(donnees_2016)):
+	if donnees_2016[i][6]=='Auxerre':
+		print('Auxerre', donnees_2016[i][9])
 		
 donnees_2021=[]
 with open('donnees_2021.csv',newline='') as csvfile:
@@ -51,3 +52,10 @@ with open('donnees_2023.csv',newline='') as csvfile:
 for i in range(len(donnees_2023)):
 	if donnees_2023[i][7]=='Auxerre':
 		print('Auxerre', donnees_2023[i][10])
+		
+
+donnees=[2008,2016,2021,2023]
+
+figure()
+plot(donnees,[35575,39456,3554,35910])
+show()
